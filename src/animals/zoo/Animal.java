e02b7081961224c469cf7eddb333fe6dc8631c93
emlag <edl926@g.harvard.edu>
2019-09-09 07:51:28 +0800

    Change Animal abstract class implementation.

--- src/animals/zoo/Animal.java
+++ src/animals/zoo/Animal.java
@@ -3,5 +3,39 @@ package animals.zoo;
 import animals.iface.Sleepable;
 
 public abstract class Animal implements Sleepable{
-    
+    private String name;
+    private int height;
+    private int weight;
+
+    public Animal()
+    {
+        this.name = "bob";
+        this.height = 5;
+        this.weight = 10;
+    }
+
+
+    public Animal(String userName, int weight, int height)
+    {
+        this.name = userName;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    @Override
+    public String goToSleep() {
+        return "zzzzz";
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
